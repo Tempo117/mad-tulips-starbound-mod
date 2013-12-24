6584c2ab5fc420b65c399fd792e1e7c23414ae70
malte.sengelmann@gmail.com <malte.sengelmann@gmail.com@d0dc7b2e-b045-4198-e669-37e27cd5ae53>
2013-12-24 00:22:59 +0000

    - should be able to fly back into the ship from below now - zero gravity doesnt trigger on planet
    - as a workaround to do this i had to make the first teleportre "unbreakable". that sux a bit and will be removed later once a better solution is possible.

--- branches/Grafics_Under_Development/madtulip/techs/gravity/madtulip_gravity.lua
+++ branches/Grafics_Under_Development/madtulip/techs/gravity/madtulip_gravity.lua
@@ -1,5 +1,6 @@
 function init()
 	tech.setAnimationState("dummy_nothing", "off");
+	We_are_in_ZERO_gravity = false;
 end
 
 function input(args)
@@ -26,64 +27,33 @@ function input(args)
 end
 
 function update(args)
-	local Range = 4; -- range of blocks around the player that need to be void to be "in space"
-	if (determine_if_ZERO_gravity(Range)) then
-		use_ZERO_gravity_movement();
+
+	if not (We_are_in_ZERO_gravity) then
+		-- try to get to ZERO gravity
+		local Range = 4; -- range of blocks around the player that need to be void to be "in space"
+		if (determine_if_ZERO_gravity(Range)) then
+			We_are_in_ZERO_gravity = true;
+			use_ZERO_gravity_movement();
+		end
 	end
+	
+	if (We_are_in_ZERO_gravity) then
+		-- try to get back to normal
+		local Range = 3; -- range of blocks around the player that need to be void to be "in space"
+		if (determine_if_ZERO_gravity_should_end(Range)) then
+			We_are_in_ZERO_gravity = false;
+		else
+			use_ZERO_gravity_movement();
+		end
+	end
+	
 	return 0;
 end
 
-function determine_if_ZERO_gravity(Range)
-
---world.logInfo ("START");
---for cur_X = 1,10,1 do
---	for cur_Y = 1,10,1 do
-		--world.logInfo ({world.material({cur_X,cur_Y}, "foreground")});
-	--end
---end
---world.logInfo ("END");
-
---	if(world.material({1,1}, "background") ~= "apexshipdetails") then
-		--world.logInfo ("RETURNING");
-		--return false;
-	--end
-
---world.logInfo ("START");
-	--local We_are_in_Shipworld = false;
-	--local Flag_Ids  = world.objectQuery (tech.position(),5000,{name = "madtulip_flag_shipworld"});
---world.logInfo ({Flag_Ids});
-	-- Perform scan for hull breach using each vents origin as point to start an individual scan
-	--for _, Flag_Id in pairs(Flag_Ids) do
---world.logInfo ({Flag_Id});
-		--world.logInfo ({world.entityPosition (Flag_Id)});
---world.logInfo ({world.logInfo ({world.entityPosition (Flag_Id)});});
-		--world.entityPosition (Flag_Id);
-		--world.logInfo ("HIT!");
-		--We_are_in_Shipworld = true;
-	--end
-	
-	--if (We_are_in_Shipworld ~= true) then
---world.logInfo ("RETURNING");
-		--return false;
-	--end
-
-
-
-	--if (storage ~= nil) then
-	--	world.logInfo ("1)");
---		if (storage.Flags ~= nil) then
-			--world.logInfo ("2)");
-		--	world.logInfo ({storage.Flags.this_is_the_shipworld});	
-	--		if not(storage.Flags.this_is_the_shipworld == true) then
---world.logInfo ("--- shipworld FLAG not found ---");
-		--		return 0;
-	--		end
---		end
-	--end
-
-	local Origin = tech.position();
+function determine_if_ZERO_gravity_should_end(Range)
 	-- we assume Zero gravity unless we find blocks in the vicinity
-	local Zero_gravity = true;
+	local Zero_gravity_ends = true;
+	local Origin = tech.position();
 	for cur_X = -Range, Range, 1 do
 		for cur_Y = -Range, Range, 1 do
 			local cur_abs_Position = {};
@@ -92,7 +62,46 @@ function determine_if_ZERO_gravity(Range)
 			-- Y coordinate where to get data
 			cur_abs_Position[2] = Origin[2] + cur_Y;
 			
-			-- get block data at that location only if there is any block to minimize output structs size
+			if ((world.material(cur_abs_Position, "foreground") == nil) and (world.material(cur_abs_Position, "background") == nil)) then
+				-- write the findings at this position to output variable
+				Zero_gravity_ends = false;
+			end
+		end	
+	end
+	
+	return Zero_gravity_ends;
+end
+
+function determine_if_ZERO_gravity(Range)
+
+	-- dirty dirty workaround: This searches if a teleporter is at that very location
+	-- this is the case for the fixed teleporter in the ship.
+	-- i didn't know any other way to degine the ship world.
+	local Teleporter_found = false;
+	local TeleporterIds = world.entityQuery ({1026,1016}, 1);
+	-- loop over one object, brilliant
+	for _, TeleporterId in pairs(TeleporterIds) do
+		if (world.entityName(TeleporterId) == "madtulip_teleporter") then
+			Teleporter_found = true;
+		end
+	end
+	if not(Teleporter_found) then
+		-- on planet, ZERO gravity is not active
+		return false;
+	end
+	
+	
+	-- we assume Zero gravity unless we find blocks in the vicinity
+	local Zero_gravity = true;
+	local Origin = tech.position();
+	for cur_X = -Range, Range, 1 do
+		for cur_Y = -Range, Range, 1 do
+			local cur_abs_Position = {};
+			-- X coordinate where to get data
+			cur_abs_Position[1] = Origin[1] + cur_X;
+			-- Y coordinate where to get data
+			cur_abs_Position[2] = Origin[2] + cur_Y;
+			
 			if not((world.material(cur_abs_Position, "foreground") == nil) and (world.material(cur_abs_Position, "background") == nil)) then
 				-- write the findings at this position to output variable
 				Zero_gravity = false;
