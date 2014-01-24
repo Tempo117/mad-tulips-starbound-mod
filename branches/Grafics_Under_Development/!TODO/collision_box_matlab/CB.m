26fd009a824d6536d12f4a689a9481a1c993a376
malte.sengelmann@gmail.com <malte.sengelmann@gmail.com@d0dc7b2e-b045-4198-e669-37e27cd5ae53>
2014-01-24 01:07:16 +0000

    - mining pod thrusters aligned

--- branches/Grafics_Under_Development/!TODO/collision_box_matlab/CB.m
+++ branches/Grafics_Under_Development/!TODO/collision_box_matlab/CB.m
@@ -8,13 +8,3 @@ Y = Y - hight/2;
 X = X/8
 Y = Y/8;
 Y = Y + 0.25 % player hight offset ...
-
-width = 160;
-hight = 68;
-X = [0 ,29,60,146,160,155,129,106,63,54,43,54,0 ];
-Y = [15,9 , 4,  4, 10, 27, 53, 68,68,58,58,53,49];
-X = X - width/2;
-Y = Y - hight/2;
-X = X/8;
-Y = Y/8;
-Y = Y + 0.25 % player hight offset ...   
