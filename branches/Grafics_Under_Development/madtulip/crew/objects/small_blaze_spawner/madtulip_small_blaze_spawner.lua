637ba26a5d234148ae9b889e0fe080257aea968b
malte.sengelmann@gmail.com <malte.sengelmann@gmail.com@d0dc7b2e-b045-4198-e669-37e27cd5ae53>
2014-03-01 15:47:58 +0000

    - fire and extinguishers updates - not working so far

---
+++ branches/Grafics_Under_Development/madtulip/crew/objects/small_blaze_spawner/madtulip_small_blaze_spawner.lua
@@ -0,0 +1,29 @@
+function init(virtual)
+	if not virtual then
+		entity.setInteractive(true);
+	end
+end
+
+function onInteraction(args)
+	if not is_shipworld() then return end
+
+	local player_ID   = args["sourceId"]
+	local player_race = world.entitySpecies(player_ID);
+
+	-- spawn NPC of users race
+	world.spawnMonster ("madtulip_blaze_small",entity.toAbsolutePosition({ 0.0, 2.0 }),{persistent = true});
+	
+	-- kill self
+	entity.smash()
+end
+
+function is_shipworld()
+	local info = world.info()
+	if info.name ~= ""then
+		-- planet
+		return false
+	else
+		-- shipworld
+		return true
+	end
+end
