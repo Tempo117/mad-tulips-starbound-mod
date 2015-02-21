840ac92efe127f05a3ecd9c1534bed9fffc1fc8d
malte.sengelmann <malte.sengelmann@gmail.com>
2015-02-21 13:11:43 +0000

    - applied our Ship Suit Tech effects (hosting our scripts like No gravity in space) to all later suits in the game (breath,cold,heat and radiationprotection)

---
+++ madtulip/tech/skins/breathprotection/madtulip_breathprotection.lua
@@ -0,0 +1,21 @@
+function init()
+	----- Suit -----
+	status.setPersistentEffects("breathprotectionTech", {{stat = "breathProtection", amount = 1}})
+	
+	-- Here we remove the invulnerability of the player on the Shipworld when he first equipps a suit
+	world.setProperty("invinciblePlayers",false)
+  
+	No_Gravity_In_Space_init();
+end
+
+function uninit()
+	status.clearPersistentEffects("breathprotectionTech")
+end
+
+function input(args)
+	No_Gravity_In_Space_input(args);
+end
+
+function update(args)
+	No_Gravity_In_Space_update(args);
+end
