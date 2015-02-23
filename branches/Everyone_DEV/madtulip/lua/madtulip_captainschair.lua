5fdc4814e689b6480f73c2db5dd0d945af6a568b
malte.sengelmann@gmail.com <malte.sengelmann@gmail.com@d0dc7b2e-b045-4198-e669-37e27cd5ae53>
2015-02-23 13:08:51 +0000

    - the navigation menue from the captainschair can be called from lua. this is huge. it allows us to code any codition before the player can use that menue. like "have at least 1 thruster for every 100 blocks worth of ship mass or you cant fly". the same principle will probably also work for techstation and the like. this allows to do a lot of stuff.

---
+++ branches/Everyone_DEV/madtulip/lua/madtulip_captainschair.lua
@@ -0,0 +1,14 @@
+function init()
+	-- world.logInfo ("Cpatainchair Init")
+end
+
+function update(dt)
+end
+
+function onInteraction(args)
+	-- world.logInfo ("Cpatainchair onInteraction")
+	return "openCockpitInterface"
+end
+
+function uninit()
+end
